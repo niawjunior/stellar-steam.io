95260985d0bd4197afc552ed01da784bd5a29ec9
niawkung <niawkung2@gmail.com>
2018-10-25 01:02:32 +0700

    :tada:

---
+++ app.js
@@ -0,0 +1,35 @@
+var payments = server.payments().forAccount(accountId);
+var payments_receive = server.payments().forAccount(accountId);
+
+function getData() {
+    document.querySelector("#list").innerHTML = ""
+    payments_receive.order("desc")
+    .call()
+    .then(function (data) {
+        data.records.map(function(item) {
+            if(item.type === 'payment') {
+                var li = document.createElement("li");
+                li.innerHTML = `from: ${item.from} amount: ${item.amount}`
+                document.querySelector("#list").appendChild(li);
+            }
+        });
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
+}
+payments
+    .cursor('now')
+    .stream({
+        onmessage: function(payment) {
+            if (payment.to !== accountId) {
+            return;
+            } else {
+                alert("New Transaction.");
+                getData();
+            }
+        },
+        onerror: function(error) {
+            console.error('Error in payment stream');
+        }
+});
---
+++ stellar.config.js
@@ -0,0 +1,2 @@
+var server = new StellarSdk.Server('https://horizon-testnet.stellar.org');
+var accountId = 'GB4C7X2QLVNGBHEWFTMQJZAQ3TG57RD6QAN2OGMHUSD73AB25RUA7VBG';
